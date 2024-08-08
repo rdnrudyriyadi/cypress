1657371a80b4bd8be1d17a7a1ccc2ca9b7410311
rudyriyadi <rudy.riyadi@gmail.com>
2024-08-08 07:25:47 +0700

    Add files via upload

--- FirstCypressTest.cy.js
+++ FirstCypressTest.cy.js
@@ -70,15 +70,22 @@ describe('Login Test for Jubelio', () => {
         return true
     })
 
+
     // Edit stock
+    cy.wait (5000)
     cy.get('a.MuiTypography-root.MuiTypography-inherit.MuiLink-root.MuiLink-underlineAlways.text-link.font-weight-bold.css-x8nsji')
-    .contains('ADJ-000000014').should('be.visible').click()
+    .contains('ADJ-000000045') 
+    .should('be.visible') 
+    .click()
 
     // Click the Edit button
+    cy.wait(2000)
     cy.get('button.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary')
-      .contains('Edit').click();
+      .contains('Edit') 
+      .click(); 
 
     //Edit stock
+    cy.wait (2000)
     cy.get('span.css-vrer1y').type('100');
 
     //Click Save button
@@ -90,13 +97,12 @@ describe('Login Test for Jubelio', () => {
     cy.get('[data-testid="CloseIcon"]').first().click();
 
     //Search Order
-    cy.get('.MuiOutlinedInput-root').first().type('ADJ-000000014')
+    cy.wait(5000) 
+    cy.get('.MuiOutlinedInput-root').first().type('ADJ-000000045')
     cy.get('.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-fullWidth.MuiButtonBase-root.css-1qelgoy')
     .click();
 
-   
-
-
+  
 
   })
 })
