eb11a676b49aaf3c468af478734b315ab4435445
rudyriyadi <rudy.riyadi@gmail.com>
2024-08-09 11:13:23 +0700

    Add files via upload

--- FirstCypressTestPenjualan.cy.js
+++ FirstCypressTestPenjualan.cy.js
@@ -6,10 +6,10 @@ describe('Login Test for Jubelio', () => {
     cy.visit('https://v2.jubelio.com/auth/login/')
 
     // Fill the field email/username
-    cy.get('input[name="email"]').type('rudyriyadi@gmail.com')
+    cy.get('input[name="email"]').type('rudy.riyadi@gmail.com')
 
     // Fill the field password
-    cy.get('input[name="password"]').type('4sdfgh1!')
+    cy.get('input[name="password"]').type('4sdfgh!!')
 
     // Click login button
     cy.get('button[type="submit"]').click()
@@ -43,50 +43,61 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     cy.visit('https://v2.jubelio.com/sales/transactions/orders')
     cy.get('[data-testid="ChevronRightIcon"]').click()
 
-    // Create New Stock
+    // Create New Sales Order
     cy.wait(5000) // Menunggu 5 detik
     cy.contains('button', 'Tambah Baru').click() 
+     
+     cy.get('input[placeholder="Pilih pelanggan"]').type('Akulaku');
+     cy.get('li').contains('Akulaku').click();
 
+     cy.get('input[placeholder="No. ref"]').type('001');
 
+     cy.get('input[placeholder="Pilih lokasi"]').type('Pusat');
+     cy.get('li').contains('Pusat').click();
 
-    /*cy.get('[data-testid="ArrowDropDownIcon"]').click() */
-    cy.get('[data-testid="ArrowDropDownIcon"]').first().click();
-    // Ensure the dropdown list is visible
-    cy.get('.MuiAutocomplete-listbox').should('be.visible')
-
-   
-
-     // Klik ikon dropdown dengan data-testid="ArrowDropDownIcon"
-    cy.get('svg[data-testid="ArrowDropDownIcon"]').first().click();
-
-
-
-     /*// Select an option from the dropdown list
-     cy.get('li#mui-12-option-22.MuiAutocomplete-option', { timeout: 10000 })
-     .should('be.visible')
-     .and('contain', 'Pusat'); */
-
-     cy.get('input[placeholder="Pilih pelanggan"]')
-     .type('Akulaku')
-     .click();
-
-     cy.get('input[placeholder="Pilih lokasi"]')
-     .type('Pusat')
-     .click ();
-
-
-
-
+    
 
   // Add Product 
     cy.contains('button', 'Tambah Baru').click()
-
     cy.contains('div', 'Celana panjang pria').click()
+    
+  // Add Penerima
+    cy.get('input[placeholder="Masukkan nama penerima"]').type('Akulaku');
+    cy.get('input[placeholder="Masukkan no telepon"]').type('08197563706');
+
+    cy.get('button[title="save"').click();
 
 
-    // Wait for the specific button to be visible
-    cy.get('#content > div > div > form > div > div.title-card.w-100 > div.MuiContainer-root.pt-3.pb-3.d-flex.justify-content-between.title-card-container.css-10ur324 > div.css-vb6e92 > button.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButtonBase-root.ml-1.shadow-none.text-white.btn-outline-primary.false.css-1hw9j7s')
-      .should('be.visible').click()
+  // Edit Sales Order
+  cy.contains('button', 'Semua').click();
+
+  cy.contains('span.font-weight-lightbold', 'SO-000000008')
+  .should('be.visible')
+  .click();
+
+  cy.get('button[title="edit"').click();
+
+  cy.wait(2000); // menunggu 2 detik
+  cy.get('input[placeholder="Pilih pelanggan"]').type('BLIBLI');
+  cy.get('li').contains('BLIBLI').click();
+
+  cy.get('input[placeholder="No. ref"]').type('002');
+
+  // Save
+  cy.get('button[title="save"').click();
+
+  //Cari Pesanan
+  cy.get('input[placeholder="Cari pesanan"]').type('SO-000000008');
+
+  cy.get('button.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-fullWidth.MuiButtonBase-root.css-1qelgoy')
+  .click();
+
+
+
+
+
+
+
 
    /* // Optionally, set up any global configurations or intercepts
     Cypress.on('uncaught:exception', (err, runnable) => {
